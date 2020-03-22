283977eaa0d59c68e486d8105d8d58601c18f171
Ian Kelly <IanKelly134@gmail.com>
2020-03-22 15:16:25 -0600

    March 22nd changes #2

--- BittleBattleBaseballWebAPI/Startup.cs
+++ BittleBattleBaseballWebAPI/Startup.cs
@@ -29,7 +29,7 @@ namespace BittleBattleBaseballWebAPI
                 options.AddPolicy(MyAllowSpecificOrigins,
                 builder =>
                 {
-                    builder.WithOrigins("http://localhost:4200", "http://bittlebattlebaseball.azurewebsites.net", "https://bittlebattlebaseball.azurewebsites.net");
+                    builder.WithOrigins("http://localhost:4200", "https://bittlebattlebaseball.azurewebsites.net", "http://bittlebattlebaseball.azurewebsites.net");
                 });
             });
 
