c4c8712e45a93b03327927507a5636a8103577ec
Ian Kelly <IanKelly134@gmail.com>
2020-03-22 20:29:13 -0600

    March 22nd #4

--- BittleBattleBaseballWebAPI/Controllers/TeamController.cs
+++ BittleBattleBaseballWebAPI/Controllers/TeamController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using BittleBattleBaseball.ApplicationService;
 using BittleBattleBaseball.Models.ViewModels;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BittleBattleBaseballWebAPI.Controllers
